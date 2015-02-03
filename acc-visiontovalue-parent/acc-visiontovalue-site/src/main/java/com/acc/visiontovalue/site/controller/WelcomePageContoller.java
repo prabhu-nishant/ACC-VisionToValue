a75f29beb374cfda5caa207ec13e1e0fa3de5fc8
nishant-prabhu <nishantr.prabhu@bestbuy.com>
2015-02-03 17:35:33 +0530

    Changes

--- acc-visiontovalue-parent/acc-visiontovalue-site/src/main/java/com/acc/visiontovalue/site/controller/WelcomePageContoller.java
+++ acc-visiontovalue-parent/acc-visiontovalue-site/src/main/java/com/acc/visiontovalue/site/controller/WelcomePageContoller.java
@@ -1,7 +1,8 @@
-package com.acc.vtv.site.controller;
+package com.acc.visiontovalue.site.controller;
 
 import javax.servlet.http.HttpServletRequest;
 
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -11,10 +12,13 @@ import org.springframework.web.bind.annotation.RequestMethod;
 @RequestMapping("/welcome")
 public class WelcomePageContoller {
 
+	@Value("${acc.visiontovalue.site.basepath}")
+	private String basepath;
+
 	@RequestMapping(method=RequestMethod.GET)
 	public String loadWelcomePage(HttpServletRequest request,Model model){
 		
-				
+		model.addAttribute("base_path",basepath);	
 		return "welcome";
 	}
 	
